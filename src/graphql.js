c52e4954ac42a227a833e48f7940362cad20b9a6
Malinali <mali@devf.mx>
2019-12-10 21:02:32 -0600

    create Post

--- src/graphql.js
+++ src/graphql.js
@@ -1,11 +1,15 @@
 import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
+// import { createHttpLink } from 'apollo-link-http';
+import { createUploadLink } from 'apollo-upload-client';
 import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const API_URL = 'https://back-35.herokuapp.com';
 
-const httpLink = createHttpLink({ uri: API_URL});
+// const httpLink = createHttpLink({ uri: API_URL});
+const httpLink = createUploadLink({
+    uri: API_URL
+});
 
 const authLink = setContext((_, { headers }) => {
     const token = sessionStorage.getItem('blogToken');
